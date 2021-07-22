f8ca6382e884086b7fd2afa9933391117dc3b09f
Carolwanzuu <carolkyalo01@gmail.com>
2021-07-22 15:48:53 +0300

    redeploying to heroku

--- m_awwward/settings.py
+++ m_awwward/settings.py
@@ -177,6 +177,9 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+# Configure Django App for Heroku.
+django_heroku.settings(locals())
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
